825a581d51230347058161efc9189b3845181429
Tom <whynotavailable@protonmail.com>
2019-05-15 21:15:35 -0400

    added wierd stuff for emacs and started node thing

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("wtf")
+let ideas = {}
+
+class Node {
+    constructor(name, model = {}) {
+	this.name = name
+	Object.assign(this, model, {
+	    flows: {} // default to empty flows
+	});
+
+	if (this.idea != void 0) {
+	    let idea = ideas[this.idea] || (ideas[this.idea] = [])
+	    idea.push(this)
+	}
+    }
+
+    sitsOn(...items) {
+	for (let item of items) {
+	    this.addFlow("sitsOn", item)
+	}
+    }
+
+    addFlow(name, item) {
+	let flow = this.flows[name]
+
+	if (flow == void 0) {
+	    flow = this.flows[name] = []
+	}
+	
+	flow.push(item)
+    }
+}
+
+function addEasyFlow(name) {
+    Node.prototype[name] = function (...items) {
+	for (let item of items) {
+	    this.addFlow(name, item);    
+	}
+    }
+}
+
+addEasyFlow("sitsOn")
+addEasyFlow("connectsTo")
+addEasyFlow("dependsOn")
+
+class Service extends Node {
+}
